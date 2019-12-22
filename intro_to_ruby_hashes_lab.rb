9cdee297a1353100d1d4ac9ca97a1672ddf894c9
Olivia Madden <madden.livi@gmail.com>
2019-12-22 06:49:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,9 +26,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
+  pair = key => hash[key]
   if hash[key]
     hash[key] += 1 
   else hash[key] = 1
   end
-  
+  return pair
 end
